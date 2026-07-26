6a674bce02cff2985f82369944929fb176d0a73b
Jonathan Crum <crumja@uga.edu>
2026-07-26 09:53:10 -0400

    07/26/2026 09:53:10

--- src/domain/prefabs/PlayerPrefab.hx
+++ src/domain/prefabs/PlayerPrefab.hx
@@ -16,9 +16,10 @@ class PlayerPrefab extends Prefab {
 		entity.add(new Sprite(TK_PLAYER_01, C_WHITE, C_BLUE_1, ACTOR));
 		entity.add(new IsPlayer());
 		entity.add(new Energy(10));
-		entity.add(new Vision(60));
+		entity.add(new Vision(40));
 		entity.add(new Moniker("Player"));
 		entity.add(new FactionMember(FACTION_PLAYER));
+
 		return entity;
 	}
 }
